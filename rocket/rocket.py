25e3b29240f2287cce81bcae116c27bd2b47400b
James Dennis <james@extension.fm>
2010-07-16 15:32:12 -0400

    Moved __init__ to rocket.py

--- rocket/rocket.py
+++ rocket/rocket.py
@@ -8,19 +8,6 @@
 #
 # Props.
 
-__doc__ = """rocket - implementing api's with a quickness
-
-For more information, see: http://github.com/extensionfm/rocket
-
-Rocket has one external dependency: simplejson
-
-To install simplejson:
-
-    pip install simpljson
-
-Or http://undefined.org/python/#simplejson
-"""
-
 import sys
 import time
 import struct
